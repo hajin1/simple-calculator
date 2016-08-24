2df911affa6e52a2a851e0d74b07d79df2cd5ad0
hajin1 <hajinkim@kangwon.ac.kr>
2016-08-24 15:39:11 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,4 +1,5 @@
 public class Calc{
+	public int r=0;
 	public int add(int x, int y){ //add method
 		return x+y;
 	}
@@ -9,5 +10,8 @@ public class Calc{
 		return x*y;
 	}
 	//testcode
+	public int dec(int d){
+		r -=d;
+	}
 }
 
