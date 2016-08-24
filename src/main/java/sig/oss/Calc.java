725fd0dd2cb50695a624b2ca9a213da0592cfba5
Kim, Hajin <hajinkim@kangwon.ac.kr>
2016-08-24 14:22:42 +0900

    update mul comment 1

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -5,7 +5,7 @@ public class Calc{
 	public int sub(int x, int y){ //sub method
 		return x-y;
 	}
-	public int mul(int x, int y){
+	public int mul(int x, int y){ //multiplication method 1 
 		return x*y;
 	}}
 }
